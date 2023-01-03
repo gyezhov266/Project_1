71bebd5577930df6ff8be648a0c60ad4f274a2ef
gyezhov266 <gregory842@revature.net>
2023-01-03 16:49:21 -0500

    Added rule file for Prometheus, updated application properties, and added alert to grafana

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
 # Used to create Project1 Docker Image
 
-docker build -t gyezhov266/project1:latest .
-docker push gyezhov266/project1:latest
+docker build -t gyezhov266/project1 .
+docker push gyezhov266/project1
