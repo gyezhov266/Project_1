613eaada5d4b6784c9a956cb67c1e7ce4adae640
gyezhov266 <gregory842@revature.net>
2023-01-13 08:57:55 -0800

    full fix

--- src/main/java/com/example/project1/repository/UserDao.java
+++ src/main/java/com/example/project1/repository/UserDao.java
@@ -1,9 +1,19 @@
 package com.example.project1.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.project1.entities.User;
 
 public interface UserDao extends JpaRepository<User,Integer>{
-    
+    Optional<User> findByusername(String name);
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into users values (default, :username, :password)", nativeQuery = true)
+    void createUser(String username, String password);
 }
